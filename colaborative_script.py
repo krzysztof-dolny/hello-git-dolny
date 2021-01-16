228c85f3db6c574eddc1a5f449bdebff3b1191d1
krzysztof-dolny <77495734+krzysztof-dolny@users.noreply.github.com>
2021-01-16 22:57:40 +0100

    Zmiany

--- colaborative_script.py
+++ colaborative_script.py
@@ -1,2 +1,7 @@
 tekst = "This is my first Python repository."
 print(tekst)
+mojaLista = []
+for literka in tekst:
+    mojaLista.append(literka)
+for n in range(len(tekst)):
+    print("The ASCII value of '" + mojaLista[n] + "' is", ord(mojaLista[n]))
